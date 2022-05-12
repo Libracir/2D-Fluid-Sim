83c9d1554b1251915574df5f7c6deadfd7e7233c
samiop <umbreon214@gmail.com>
2022-05-12 17:11:33 +0100

    Added Grid System and Basic Logic

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private float[,] grid;
+
+    public Grid(float[,] grid)
+    {
+        this.grid = grid;
+    }
+    public float GetNode(int x, int y) 
+    {
+        return grid[x, y];
+    }
+    public void SetNode(int x, int y, float value)
+    {
+        grid[x, y] = value;
+    }
+    public void SetAll(float value)
+    {
+        for (int x = 0; x < grid.GetLength(0); x++)
+        {
+            for (int y = 0; y < grid.GetLength(1); y++)
+            {
+                grid[x, y] = value;
+            }
+        }
+    }
+    public void DebugAll()
+    {
+        {
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    Debug.Log(grid[x, y]);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GridSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem : MonoBehaviour
+{
+    public int sizeX;
+    public int sizeY;
+    Grid grid;
+    void Start()
+    {
+        grid = new Grid(new float[sizeX,sizeY]);
+    }
+    void Update()
+    {
+        
+    }
+
+    //Makes a node and the four nodes directly touching it equal
+    void EquilizeNodes(int x, int y)
+    {
+        float temp = grid.GetNode(x, y) / 5f;
+        grid.SetNode(x ,y ,temp);
+        grid.SetNode(x+1, y, temp);
+        grid.SetNode(x-1, y, temp);
+        grid.SetNode(x, y+1, temp);
+        grid.SetNode(x, y-1, temp);
+    }
+}
